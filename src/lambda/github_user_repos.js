36ce03fd82504d114133f2b3e03993a068833006
Taylor <tbaustin1992@gmail.com>
2018-10-12 15:38:12 -0500

    updates

--- src/lambda/github_user_repos.js
+++ src/lambda/github_user_repos.js
@@ -1,14 +1,5 @@
-const querystring = require("querystring")
-
 exports.handler = async (event, context) => {
- // Only allow POST
- if (event.httpMethod !== 'POST') {
-  return { statusCode: 405, body: 'Method Not Allowed' }
- }
-
- // When the method is POST, the name will no longer be in the event’s
- // queryStringParameters – it’ll be in the event body encoded as a query string
- const params = querystring.parse(event.body)
+ const params = event.body
  const user = params.user || 'escaladesports'
  const url = `https://api.github.com/users/${user}/repos`
  const res = await fetch(url, {
