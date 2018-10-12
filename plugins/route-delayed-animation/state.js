8ebf29ac176a84810fb14d19cf0a6abd273b3c09
Matt Suhay <matthew.suhay@gmail.com>
2018-10-12 13:06:22 -0500

    Adding lambda base

--- plugins/route-delayed-animation/state.js
+++
@@ -1,7 +0,0 @@
-import { State } from 'statable'
-
-const state = new State({
-	delayed: false,
-})
-
-export default state
--- plugins/search/index.js
+++
@@ -1,3 +0,0 @@
-export { default as fetchIndex } from './fetch-index'
-export { default as search } from './search'
-export { default as SearchResults } from './search-results'
