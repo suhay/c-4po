bf4cf052f3f17cf3202da7e57980d8500b63d6d0
Taylor <tbaustin1992@gmail.com>
2018-10-12 16:56:53 -0500

    updates

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -1,15 +1,16 @@
 const fetch = require('isomorphic-fetch')
 
 exports.handler = async (event, context) => {
+ console.log(Date.now(), `API CALLED`)
  const params = JSON.parse(event.body)
  const func = params.queryResult.intent.displayName || 'hello_name'
 
  const url = `https://c-4po.netlify.com/.netlify/functions/${func}`
  const res = await fetch(url, {
   method: 'POST',
-  body: params
+  body: event.body
  }).then(res => res.json())
- console.log(Date.now(), `API CALLED`)
+
  return {
   statusCode: 200,
   body: res
