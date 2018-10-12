3c139cd29fcba21cbb92a8b07f7a5478e7f6b2b2
Taylor <tbaustin1992@gmail.com>
2018-10-12 16:41:24 -0500

    updates

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -2,13 +2,12 @@ const fetch = require('isomorphic-fetch')
 
 exports.handler = async (event, context) => {
  const params = JSON.parse(event.body)
- console.log(params)
  const func = params.queryResult.intent.displayName || 'hello_name'
 
  const url = `https://c-4po.netlify.com/.netlify/functions/${func}`
  const res = await fetch(url, {
   method: 'POST',
-  body: event.body
+  body: JSON.stringify(event.body)
  }).then(res => res.json())
 
  console.log(res)
--- src/lambda/github_user_repos.js
+++ src/lambda/github_user_repos.js
@@ -2,7 +2,6 @@ const fetch = require('isomorphic-fetch')
 
 exports.handler = async (event, context) => {
  const params = JSON.parse(event.body)
- console.log(params)
  const user = params.queryResult.parameters.user || 'escaladesports'
  const url = `https://api.github.com/users/${user}/repos`
  const res = await fetch(url, {
