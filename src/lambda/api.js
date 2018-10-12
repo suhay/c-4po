19a13993f32cf7bd98285fd85d932b27ae8e3cba
Matt Suhay <matthew.suhay@gmail.com>
2018-10-12 15:53:56 -0500

    More test consoles

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -1,20 +1,16 @@
-const querystring = require("querystring")
-
 exports.handler = async (event, context) => {
-console.log(event)
-console.log(context)
-	const params = querystring.parse(event.body)
-	const func = params.intent_name || 'hello_name'
-
+	const params = event.body
+	const func = params.parameters.intent.displayName || 'hello_name'
+	console.log(params)
 	const url = `/.netlify/functions/${func}`
 	const res = await fetch(url, {
-		method: 'POST',
-		body: event.body,
+	 method: 'POST',
 	}).then(res => res.json())
  
 	console.log(res)
 	return {
 	 statusCode: 200,
-	 body: res
+	 body: res.length
 	}
-}
+ }
+ 
