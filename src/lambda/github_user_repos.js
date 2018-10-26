73f107fd283d15e70d0d97bd7dbea57e8cad0ff1
Taylor <tbaustin1992@gmail.com>
2018-10-26 16:35:15 -0500

    update

--- src/lambda/github_user_repos.js
+++ src/lambda/github_user_repos.js
@@ -12,6 +12,7 @@ exports.handler = async (event, context) => {
    Accept: `application/vnd.github.v3+json`
   }
  })
+ console.log(Object.keys(res))
  return {
   statusCode: 200,
   body: JSON.stringify({
