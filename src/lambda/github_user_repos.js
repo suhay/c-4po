7a7ac4c6f6189ddff49ab829e3a43f53ff532d28
Taylor <tbaustin1992@gmail.com>
2018-10-26 15:13:42 -0500

    update

--- src/lambda/github_user_repos.js
+++ src/lambda/github_user_repos.js
@@ -4,6 +4,7 @@ const axios = require('axios')
 exports.handler = async (event, context) => {
  const params = JSON.parse(event.body)
  const user = params.queryResult.parameters.user || 'escaladesports'
+ console.log(user)
  const url = `https://api.github.com/users/tbaustin/${user}`
  const res = await axios({
   method: 'GET',
