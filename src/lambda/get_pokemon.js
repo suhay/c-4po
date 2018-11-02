61c3ce00f402a5bf4f6d5956f005ec4c800208d7
Taylor <tbaustin1992@gmail.com>
2018-11-02 15:20:55 -0500

    update

--- src/lambda/get_pokemon.js
+++ src/lambda/get_pokemon.js
@@ -13,59 +13,77 @@ exports.handler = async (event, context) => {
  return {
   statusCode: 200,
   body: JSON.stringify({
+   fulfillmentText: 'This is a text response',
+   fulfillmentMessages: [
+    {
+     card: {
+      title: 'card title',
+      subtitle: 'card text',
+      imageUri:
+       'https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png',
+      buttons: [
+       {
+        text: 'button text',
+        postback: 'https://assistant.google.com/'
+       }
+      ]
+     }
+    }
+   ],
+   source: 'example.com',
    payload: {
-    // {
-    //  card: {
-    //   title: res.data.name,
-    //   subtitle: 'stats',
-    //   imageUri: res.data.sprites.front_default,
-    //   buttons: [
-    //    {
-    //     text: 'Check stats on Bulbapedia',
-    //     postback: `https://bulbapedia.bulbagarden.net/wiki/${
-    //      res.data.name
-    //     }_(Pok%C3%A9mon)`
-    //    }
-    //   ]
-    //  }
-    // },
     google: {
-     richResponses: {
+     expectUserResponse: true,
+     richResponse: {
       items: [
        {
-        carouselBrowse: {
-         items: [
-          {
-           title: '1',
-           description: 'here',
-           image: {
-            imageUri: res.data.sprites.front_default,
-            accessibilityText: `pokemon`
-           }
-          },
-          {
-           title: '1',
-           description: 'here',
-           image: {
-            imageUri: res.data.sprites.front_default,
-            accessibilityText: `pokemon`
-           }
-          },
-          {
-           title: '1',
-           description: 'here',
-           image: {
-            imageUri: res.data.sprites.front_default,
-            accessibilityText: `pokemon`
-           }
-          }
-         ]
+        simpleResponse: {
+         textToSpeech: 'this is a simple response'
         }
        }
       ]
      }
+    },
+    facebook: {
+     text: 'Hello, Facebook!'
+    },
+    slack: {
+     text: 'This is a text response for Slack.'
+    }
+   },
+   outputContexts: [
+    {
+     name:
+      'projects/${PROJECT_ID}/agent/sessions/${SESSION_ID}/contexts/context name',
+     lifespanCount: 5,
+     parameters: {
+      param: 'param value'
+     }
+    }
+   ],
+   followupEventInput: {
+    name: 'event name',
+    languageCode: 'en-US',
+    parameters: {
+     param: 'param value'
     }
    }
   })
  }
 }
+
+// {
+//  card: {
+//   title: res.data.name,
+//   subtitle: 'stats',
+//   imageUri: res.data.sprites.front_default,
+//   buttons: [
+//    {
+//     text: 'Check stats on Bulbapedia',
+//     postback: `https://bulbapedia.bulbagarden.net/wiki/${
+//      res.data.name
+//     }_(Pok%C3%A9mon)`
+//    }
+//   ]
+//  }
+// },
