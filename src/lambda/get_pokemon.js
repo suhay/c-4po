cdcdf60de629d3447198239e2367b9ef5869ec79
Taylor <tbaustin1992@gmail.com>
2018-11-02 14:21:31 -0500

    update

--- src/lambda/get_pokemon.js
+++ src/lambda/get_pokemon.js
@@ -13,75 +13,73 @@ exports.handler = async (event, context) => {
  return {
   statusCode: 200,
   body: JSON.stringify({
-   fulfillmentMessages: [
-    {
-     payload: {
-      google: {
-       expectUserResponse: true,
-       richResponse: {
-        items: [
-         {
-          carouselBrowse: {
-           items: [
-            {
-             title: 'Title of item 1',
-             openUrlAction: {
-              url: 'https://google.com'
-             },
-             description: 'Description of item 1',
-             footer: 'Item 1 footer',
-             image: {
-              url: 'https://developers.google.com/actions/assistant.png',
-              accessibilityText: 'Google Assistant Bubbles'
-             }
-            },
-            {
-             title: 'Title of item 2',
-             openUrlAction: {
-              url: 'https://google.com'
-             },
-             description: 'Description of item 2',
-             footer: 'Item 2 footer',
-             image: {
-              url: 'https://developers.google.com/actions/assistant.png',
-              accessibilityText: 'Google Assistant Bubbles'
-             }
-            }
-           ]
+   payload: {
+    google: {
+     expectUserResponse: true,
+     richResponse: {
+      items: [
+       {
+        carouselBrowse: {
+         items: [
+          {
+           title: 'Title of item 1',
+           openUrlAction: {
+            url: 'https://google.com'
+           },
+           description: 'Description of item 1',
+           footer: 'Item 1 footer',
+           image: {
+            url: 'https://developers.google.com/actions/assistant.png',
+            accessibilityText: 'Google Assistant Bubbles'
+           }
+          },
+          {
+           title: 'Title of item 2',
+           openUrlAction: {
+            url: 'https://google.com'
+           },
+           description: 'Description of item 2',
+           footer: 'Item 2 footer',
+           image: {
+            url: 'https://developers.google.com/actions/assistant.png',
+            accessibilityText: 'Google Assistant Bubbles'
+           }
           }
-         }
-        ]
-       },
-       userStorage: '{"data":{}}'
-      }
-     },
-     outputContexts: [
-      {
-       name:
-        'projects/temperatureconvertersample/agent/sessions/518488a5-09f6-4a36-8950-942f595b70b8/contexts/_actions_on_google',
-       lifespanCount: 99,
-       parameters: {
-        data: '{}'
+         ]
+        }
        }
-      }
-     ]
+      ]
+     },
+     userStorage: '{"data":{}}'
     }
-    // {
-    //  card: {
-    //   title: 'Stats for: ',
-    //   subtitle: res.data.name,
-    //   imageUri: res.data.sprites.front_default,
-    //   buttons: [
-    //    {
-    //     text: 'Check stats on Bulbapedia',
-    //     postback: `https://bulbapedia.bulbagarden.net/wiki/${
-    //      res.data.name
-    //     }_(Pok%C3%A9mon)`
-    //    }
-    //   ]
-    //  }
-    // }
+   },
+   outputContexts: [
+    {
+     name:
+      'projects/temperatureconvertersample/agent/sessions/518488a5-09f6-4a36-8950-942f595b70b8/contexts/_actions_on_google',
+     lifespanCount: 99,
+     parameters: {
+      data: '{}'
+     }
+    }
    ]
   })
  }
 }
+// fulfillmentMessages: [
+// {
+//  card: {
+//   title: 'Stats for: ',
+//   subtitle: res.data.name,
+//   imageUri: res.data.sprites.front_default,
+//   buttons: [
+//    {
+//     text: 'Check stats on Bulbapedia',
+//     postback: `https://bulbapedia.bulbagarden.net/wiki/${
+//      res.data.name
+//     }_(Pok%C3%A9mon)`
+//    }
+//   ]
+//  }
+// }
+// ]
