16059f230b7f7527fff5c2a90ec5918f96aacc70
Matt Suhay <matthew.suhay@gmail.com>
2018-11-02 16:47:19 -0500

    Setting cors headers

--- src/lambda/giphy.js
+++ src/lambda/giphy.js
@@ -14,6 +14,7 @@ exports.handler = async (event, context) => {
 
 	return {
 		statusCode: 200,
+		headers: { "Access-Control-Allow-Origin": "*" },
 		body: JSON.stringify({
 			"fulfillmentMessages": [
 				{
