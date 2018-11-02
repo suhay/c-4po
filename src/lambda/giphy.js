93e51ba517333b91a76f6ac3d5896116b7ce7848
Matt Suhay <matthew.suhay@gmail.com>
2018-11-02 15:07:17 -0500

    Fixing giphy api

--- src/lambda/giphy.js
+++ src/lambda/giphy.js
@@ -2,21 +2,20 @@ const axios = require('axios')
 require('dotenv').config()
 
 exports.handler = async (event, context) => {
- const url = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API}&rating=pg-13&fmt=json`
- const res = await axios({
-  method: 'GET',
-  url,
-  headers: {
-	 Accept: `application/json`,
-  }
- })
+	console.log(process.env.GIPHY_API)
+	const url = `http://api.giphy.com/v1/gifs/random?api_key=${process.env.GIPHY_API}&rating=pg-13&fmt=json`
+	const res = await axios({
+		method: 'GET',
+		url,
+		headers: {
+			Accept: `application/json`,
+		}
+	})
 
- return {
-  statusCode: 200,
-  body: JSON.stringify({
-   fulfillmentText: `${res.data.embed_url}`
-  })
- }
+	return {
+		statusCode: 200,
+		body: JSON.stringify({
+			fulfillmentText: `${res.data.embed_url}`
+		})
+	}
 }
-
-
