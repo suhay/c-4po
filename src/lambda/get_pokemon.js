13a5802677d05251bc7d66ee5babf46e61c101d5
Taylor <tbaustin1992@gmail.com>
2018-11-02 14:31:41 -0500

    update

--- src/lambda/get_pokemon.js
+++ src/lambda/get_pokemon.js
@@ -14,58 +14,53 @@ exports.handler = async (event, context) => {
   statusCode: 200,
   body: JSON.stringify({
    payload: {
-    google: {
-     expectUserResponse: true,
-     richResponse: {
+    "google": {
+     "expectUserResponse": true,
+     "richResponse": {
       items: [
        {
-        carouselBrowse: {
-         items: [
+        "carouselBrowse": {
+         "items": [
           {
-           title: 'Title of item 1',
-           openUrlAction: {
-            url: 'https://google.com'
-           },
-           description: 'Description of item 1',
-           footer: 'Item 1 footer',
-           image: {
-            url: 'https://developers.google.com/actions/assistant.png',
-            accessibilityText: 'Google Assistant Bubbles'
-           }
+            "description": "Option One Description",
+            "image": {
+              "url": "http://imageOneUrl.com",
+              "accessibilityText": "Image description for screen readers"
+            },
+            "optionInfo": {
+              "key": "itemOne",
+              "synonyms": [
+                "thing one",
+                "object one"
+              ]
+            },
+            "title": "Option One Title"
           },
           {
-           title: 'Title of item 2',
-           openUrlAction: {
-            url: 'https://google.com'
-           },
-           description: 'Description of item 2',
-           footer: 'Item 2 footer',
-           image: {
-            url: 'https://developers.google.com/actions/assistant.png',
-            accessibilityText: 'Google Assistant Bubbles'
-           }
+            "description": "Option Two Description",
+            "image": {
+              "url": "http://imageTwoUrl.com",
+              "accessibilityText": "Image description for screen readers"
+            },
+            "optionInfo": {
+              "key": "itemTwo",
+              "synonyms": [
+                "thing two",
+                "object two"
+              ]
+            },
+            "title": "Option Two Title"
           }
-         ]
+        ],
         }
        }
       ]
-     },
-     userStorage: '{"data":{}}'
-    }
-   },
-   outputContexts: [
-    {
-     name:
-      'projects/temperatureconvertersample/agent/sessions/518488a5-09f6-4a36-8950-942f595b70b8/contexts/_actions_on_google',
-     lifespanCount: 99,
-     parameters: {
-      data: '{}'
      }
     }
-   ]
-  })
+   }
  }
 }
+
 // fulfillmentMessages: [
 // {
 //  card: {
