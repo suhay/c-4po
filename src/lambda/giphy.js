7e67e01d6398e9dc75edc801bca62fe6cb71f339
Matt Suhay <matthew.suhay@gmail.com>
2018-11-02 15:26:36 -0500

    Adjusting for axios get call

--- src/lambda/giphy.js
+++ src/lambda/giphy.js
@@ -9,7 +9,13 @@ exports.handler = async (event, context) => {
 		fmt: `json`
 	}
 	const url = `http://api.giphy.com/v1/gifs/random`
-	const res = await axios.get(url, querystring.stringify(data))
+	const res = await axios.get(url, {
+		params: {
+			api_key: process.env.GIPHY_API,
+			rating: `pg-13`,
+			fmt: `json`
+		}
+	})
 	console.log(res)
 
 	return {
