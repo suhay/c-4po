6d507d5dfc38aad798e096eecbf5bbe9eec7f507
Matt Suhay <matthew.suhay@gmail.com>
2018-11-02 19:11:45 -0500

    Adding slack token

--- src/lambda/api.js
+++ src/lambda/api.js
@@ -1,8 +1,16 @@
-const fetch = require('isomorphic-fetch')
 const axios = require('axios')
+require('dotenv').config()
 
 exports.handler = async (event, context) => {
  const params = JSON.parse(event.body)
+
+ if (params.token && params.challenge && params.token === process.env.SLACK_TOKEN) {
+  return {
+    statusCode: 200,
+    body: process.env.SLACK_TOKEN
+  }
+ }
+
  const func = params.queryResult.intent.displayName || 'hello_name'
  const url = `https://c-4po.netlify.com/.netlify/functions/${func}`
  console.log(`URL`, url)
